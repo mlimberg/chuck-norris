a183f9e6bd17e2db935089be45467f33960b303a
mlimberg <limbergmike@gmail.com>
2017-01-23 15:23:59 -0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import './styles';
+import Header from './components/Header/Header';
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+
+      <Header />
+    </div>
+  );
+}
+
+render(<App />, document.querySelector('.application'));
