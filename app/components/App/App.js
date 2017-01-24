011ee4ea3143da6026af0c688ecc7f2a61d89b20
mlimberg <limbergmike@gmail.com>
2017-01-23 23:01:33 -0700

    successful api call pulling in single feature joke

---
+++ app/components/App/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header';
+import JokeContainer from '../JokeContainer/JokeContainer';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      featureJoke: '',
+      jokeArray: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.icndb.com/jokes/random/1?escape=javascript').then((response) => {
+      return response.json()
+    }).then((data) => {
+      this.setState({ featureJoke: data.value[0].joke })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <JokeContainer joke={this.state.featureJoke}/>
+      </div>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './components/Header/Header';
-import JokeContainer from './components/JokeContainer/JokeContainer';
-
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <JokeContainer />
-    </div>
-  );
-}
+import App from './components/App/App'
+// import Header from './components/Header/Header';
+// import JokeContainer from './components/JokeContainer/JokeContainer';
 
 render(<App />, document.querySelector('.application'));
