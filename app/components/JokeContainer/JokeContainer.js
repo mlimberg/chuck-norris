cc12d6936047bce72f2ea833cb6f8db918270640
mlimberg <limbergmike@gmail.com>
2017-01-23 21:25:21 -0700

    fix background color for header

--- app/components/JokeContainer/JokeContainer.js
+++ app/components/JokeContainer/JokeContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './joke-container-style';
+import JokeCard from '../JokeCard/JokeCard';
 
 const JokeContainer = (props) => {
   return (
     <div className='body'>
-
+      <JokeCard />
     </div>
   )
 }
