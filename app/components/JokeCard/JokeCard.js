54f645056814577c3fff700e7d6046852aad17fb
mlimberg <limbergmike@gmail.com>
2017-01-25 13:01:51 -0700

    add star logo to joke card

--- app/components/JokeCard/JokeCard.js
+++ app/components/JokeCard/JokeCard.js
@@ -1,11 +1,12 @@
-import React from 'react';
 import './joke-card-style';
+import React from 'react';
+import Button from '../Button/Button';
 
 const JokeCard = (props) => {
   return (
     <div className='joke-card' id={props.id}>
       {props.joke}
-      <button>Fave!</button>
+      <Button id='fave-btn' text='★' handleClick={() => console.log('fave!')} />
     </div>
   )
 }
